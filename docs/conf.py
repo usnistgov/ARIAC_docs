42e8b785b147cbf874ae06e8f322cb0f1dedf608
Justin <jalbrecht23@gmail.com>
2023-12-06 12:01:07 -0500

    initial configuration

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ARIAC_docs'
+copyright = u'works of NIST employees are not not subject to copyright protection in the United States'
+author = 'Justin Albrecht'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon',]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autoclass_content = "both"
+
+napoleon_numpy_docstring = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_show_copyright = False
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. _ARIAC_DOCS_INTRO:
+
+``ARIAC Docs``
+=============
+
+.. toctree::
+   :caption: ⏩ Getting Started
+   :maxdepth: 3
+   :hidden:
+
+   getting_started/updates.rst
+
+.. [#] Certain commercial firms and trade names are identified in this
+    document in order to specify the installation and usage procedures
+    adequately, or to provide context.  Such identification is not intended
+    to imply recommendation or endorsement by the `National Institute of
+    Standards and Technology <https://www.nist.gov>`_, nor is it intended
+    to imply that related products are necessarily the best available for
+    the purpose.
