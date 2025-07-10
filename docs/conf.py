16287e475a1105d8dfa858f21c64cb4d9d3cf701
Justin Albrecht <justin.albrecht@nist.gov>
2025-07-10 16:32:29 -0400

    changed theme again

--- docs/conf.py
+++ docs/conf.py
@@ -11,14 +11,17 @@ version = '2025.1.0'
 copyright = u'works of NIST employees are not not subject to copyright protection in the United States'
 author = 'Justin Albrecht'
 
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
+
+html_logo = "_static/nist_el_logo_color.png"
 
 html_static_path = ["_static"]
 
 html_theme_options = {
-    "light_logo": "nist_el_logo_color.png",
-    "dark_logo": "nist_el_logo_white.png",
-    "sidebar_hide_name": True,
+    "logo": {
+        "image_light": "_static/nist_el_logo_color.png",
+        "image_dark": "_static/nist_el_logo_white.png",
+    }
 }
 
 templates_path = ['_templates']
